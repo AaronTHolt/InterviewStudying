4aa03a4dad9826206f2fdbea72cc4f968dd1ab7a
AaronTHolt <holtat@colorado.edu>
2015-11-02 14:51:28 -0700

    Broken binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -36,7 +36,7 @@ def insert_node(node, data):
         else:
             insert_node(node.right, data)
     #Data less than current node data
-    elif data < node.data:
+    elif delete_data < node.data:
         if node.left == None:
             node.left = node_1(parent=node, data=data)
         else:
@@ -44,6 +44,76 @@ def insert_node(node, data):
 
 def delete_data(root, data):
     '''Delete data if it exist'''
+    if root.data == data:
+        return delete_node(root)
+    elif data < root.data:
+        if root.left:
+            delete_data(root.left, data)
+        else:
+            return False
+    else:
+        if root.right:
+            delete_data(root.right, data)
+        else:
+            return False
+
+
+
+
+# def delete_node(node):
+#     # temp_parent = node.Parent
+#     temp_node = None
+
+#     #root node?
+#     if node.Parent == None:
+#         node.data = None
+#         return True
+
+#     #no children
+#     if node.left == None and node.right == None:
+#         if node.Parent.left == node:
+#             node.Parent.left = None
+#         else:
+#             node.Parent.right = None
+
+#     #has one child
+#     elif node.right != node.left:
+#         if node.right:
+#             temp_node = node.right
+#         else:
+#             temp_node = node.left
+
+#         if node.Parent:
+#             if node.parent.left == Node:
+#                 node.parent.left = temp_node
+#             else:
+#                 node.parent.right = temp_node
+#             del nod
+#         else:
+#             self.data = temp_node.data
+#             self.left = temp_node.data
+#             self.right = temp_node.right
+
+#     #has 2 children
+#     else:
+#         parent = node
+#         successor = node.right
+        
+#         while successor.left:
+#             parent = successor.left
+#             successor = successor.left
+#         node.data = successor.data
+
+#         if parent.left == successor:
+#             parent.left = succerssor.right
+#         else:
+#             parent.right = successor.right
+
+
+
+
+
+
 
 
 
@@ -53,7 +123,9 @@ for ii in range(0,30,4):
     insert_node(r, ii)
 
 print_tree_inorder(r)
-print ''
-insert_node(r, 11)
-insert_node(r, 27)
-print_tree_inorder(r)
+# print ''
+# insert_node(r, 11)
+# insert_node(r, 27)
+# print_tree_inorder(r)
+# delete_data(r, 11)
+# print_tree_inorder(r)
